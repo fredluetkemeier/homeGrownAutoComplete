7ecd792992972b98b0503f41bd975a3cdbf8af83
Fred Luetkemeier <flpn6@mail.missouri.edu>
2020-01-10 23:29:49 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+	entry: {
+		index: './src/index.js'
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].bundle.js'
+	},
+	watch: true,
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		hot: true,
+		port: 9000
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'index.html'
+		}),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
