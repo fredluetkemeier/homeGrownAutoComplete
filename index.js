7a7bee4968e1b66fca08a4cda3c19f1cd4735ea8
Fred Luetkemeier <35507408+fredluetkemeier@users.noreply.github.com>
2020-01-15 15:23:32 -0600

    Restructured internal functions

--- index.js
+++ index.js
@@ -18,20 +18,27 @@ const onKeyUp = event => {
 
 	results.forEach(result => {
 		const item = document.createElement('li');
-		item.innerHTML = addMatchHighlighting(searchTerm, result, HIGHLIGHT_CLASS);
+		item.innerHTML = addMatchHighlighting(result, searchTerm, HIGHLIGHT_CLASS);
 
 		resultsRef.appendChild(item);
 	});
 };
 
-function addMatchHighlighting(searchTerm, result, highlightClass) {
-	const matchStart = result.toLowerCase().indexOf(searchTerm.toLowerCase());
-	const matchEnd = matchStart + searchTerm.length;
+function addMatchHighlighting(result, match, highlightClass) {
+	const [start, end] = findSubstringRange(result, match);
 
-	const highlighted = result.substring(matchStart, matchEnd);
-	const rest = result.substring(matchEnd);
+	const before = result.substring(0, start);
+	const highlighted = result.substring(start, end);
+	const rest = result.substring(end);
 
-	return `<span class="${highlightClass}">${highlighted}</span>${rest}`;
+	return `${before}<span class="${highlightClass}">${highlighted}</span>${rest}`;
+}
+
+function findSubstringRange(string, substring) {
+	const start = string.toLowerCase().indexOf(substring.toLowerCase());
+	const end = start + substring.length;
+
+	return [start, end];
 }
 
 autoComplete.setup([objectEntries], { inputId: INPUT_ID, onKeyUp });
